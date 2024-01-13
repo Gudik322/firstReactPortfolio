ef531b7c3f3cd88ab6b7675875e53b7a7fe11a05
Віктор Брижко <bryzhko.viktor98@gmail.com>
2024-01-13 19:55:40 +0200

    first commit

---
+++ src/pages/Contacts.js
@@ -0,0 +1,27 @@
+const Contacts = () => {
+    return (  
+        <main className="section">
+        <div className="container">
+                <h1 className="title-1">Contacts</h1>
+
+                <ul className="content-list">
+                    <li className="content-list__item">
+                        <h2 className="title-2">Location</h2>
+                        <p>Rivne, Ukraine</p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Telegram</h2>
+                        <p><a href="">@gud_ik</a></p>
+                    </li>
+                    <li className="content-list__item">
+                        <h2 className="title-2">Email</h2>
+                        <p><a href="mailto:webdev@protonmail.com">test@gmail.com</a></p>
+                    </li>
+                </ul>
+
+        </div>
+    </main>
+    );
+}
+ 
+export default Contacts;
